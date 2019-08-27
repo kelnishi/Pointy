aa9064f0c43799711ebd2323bd0ca01dbe975f4b
Kelvin N. Nishikawa <kelvinnishikawa@gmail.com>
2019-08-26 20:57:03 -0700

    Got some camera frames coming in.

---
+++ CameraCapture.cs
@@ -0,0 +1,98 @@
+﻿using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Threading;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace Pointy
+{
+    class CameraCapture : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // Create class-level accesible variables
+        VideoCapture capture;
+        Mat frame;
+        private Thread camera;
+
+        public bool isCameraRunning = false;
+
+        int frames = 0;
+
+        private BitmapSource currentCameraImage;
+        public BitmapSource CurrentCameraImage
+        {
+            get { return currentCameraImage; }
+            set
+            {
+                currentCameraImage = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("CurrentCameraImage"));
+            }
+        }
+        
+        // Declare required methods
+        public void Start()
+        {
+            camera = new Thread(new ThreadStart(CaptureCameraCallback));
+            camera.Start();
+        }
+
+        BitmapImage BitmapToImageSource(Bitmap bitmap)
+        {
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                BitmapImage bitmapimage = new BitmapImage();
+                bitmapimage.BeginInit();
+                bitmapimage.StreamSource = memory;
+                bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapimage.EndInit();
+                bitmapimage.Freeze();
+
+                return bitmapimage;
+            }
+        }
+
+        private void CaptureCameraCallback()
+        {
+            frame = new Mat();
+
+                   
+
+            int idx = 2;
+            capture = new VideoCapture(CaptureDevice.DShow, idx);
+            capture.Open(CaptureDevice.DShow, idx);
+            if (capture.IsOpened())
+            {
+                while (isCameraRunning)
+                {
+                    capture.Read(frame);
+
+                    var bitmap = frame.ToWriteableBitmap();
+                    bitmap.Freeze();
+
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        CurrentCameraImage = bitmap;
+                    }, System.Windows.Threading.DispatcherPriority.Render);
+                }
+            }
+        }
+
+
+        public List<int> camIdList = new List<int>();
+        private OpenCVDeviceEnumerator.OpenCVDeviceEnumerator enumerator = new OpenCVDeviceEnumerator.OpenCVDeviceEnumerator();
+
+
+        public void EnumerateCameras()
+        {
+            enumerator.EnumerateCameras(camIdList);
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,12 @@
-﻿using System;
+﻿using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -18,11 +23,31 @@ namespace Pointy
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : System.Windows.Window
     {
+        
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        CameraCapture cc;
+        
         public MainWindow()
         {
             InitializeComponent();
+            cc = new CameraCapture();
+
+            cc.EnumerateCameras();
+
+            cc.isCameraRunning = true;
+            cc.Start();
+
+            outputframe.DataContext = cc;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            cc.isCameraRunning = false;
         }
     }
 }
